199371d03f1e61109eac3a60e89a6909e5015a2d
alyalac <andreilancecamata8@gmail.com>
2026-05-11 20:31:36 +0800

    fix: proper constructor initialization

--- javaSelfStudy/src/activity/GradeAverageProgram.java
+++ javaSelfStudy/src/activity/GradeAverageProgram.java
@@ -4,14 +4,30 @@ import java.util.Scanner;
 
 public class GradeAverageProgram {
 
+    // Attributes
     String studentName;
     int studentid;
     int english;
     int math;
     int science;
     int computer;
+    int average;
 
-    public GradeAverageProgram() {
+    // Constructor
+    public GradeAverageProgram(String studentName, int studentid, int english, int math, int science, int computer,
+            int average) {
+
+        this.studentName = studentName;
+
+        this.studentid = studentid;
+        this.english = english;
+        this.math = math;
+        this.science = science;
+        this.computer = computer;
+        this.average = average;
+    }
+
+    public void inputInfo() {
 
         Scanner sc = new Scanner(System.in);
 
@@ -35,29 +51,52 @@ public class GradeAverageProgram {
 
     }
 
+    public int finalAverage() {
+        int[] grades = { this.english, this.math, this.science, this.computer };
+        int total = 0;
+
+        for (int grade : grades) {
+            total += grade;
+
+        }
+
+        this.average = total / grades.length;
+        return this.average; // final answer
+    }
+
     public void mainOutput() {
-        System.out.println("Student Grade");
 
-        System.out.println("Student Name: " + (this.studentName));
+        System.out.println("-----------------------------------");
 
-        System.out.println("Student ID: " + (this.studentid));
+        System.out.println("\nStudent Grade");
 
-        System.out.println("English: " + (this.english));
+        System.out.println("Student Name: " + this.studentName);
 
-        System.out.println("Math: " + (this.math));
+        System.out.println("Student ID: " + this.studentid);
 
-        System.out.println("Science: " + (this.science));
+        System.out.println("\nEnglish: " + this.english);
 
-        System.out.println("Computer: " + (this.computer));
+        System.out.println("Math: " + this.math);
+
+        System.out.println("Science: " + this.science);
+
+        System.out.println("Computer: " + this.computer);
+
+        System.out.println("-----------------------------------");
+
+        System.out.println("GPA: " + this.average);
 
     }
 
     public static void main(String[] args) {
 
-        GradeAverageProgram one = new GradeAverageProgram();
+        GradeAverageProgram in = new GradeAverageProgram("Unknown", 0, 0, 0, 0, 0, 0);
 
-        one.mainOutput();
+        in.inputInfo();
 
+        in.finalAverage();
+
+        in.mainOutput();
     }
 
 }

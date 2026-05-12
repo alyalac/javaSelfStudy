5f5636a1916177c47db189c8de2033caedd59882
alyalac <andreilancecamata8@gmail.com>
2026-05-12 18:42:57 +0800

    falseCondition

--- javaSelfStudy/src/javaStudy/Loops/WhileLoop.java
+++ javaSelfStudy/src/javaStudy/Loops/WhileLoop.java
@@ -1,22 +1,36 @@
 package javaStudy.Loops;
+// count++, count = count + 1 — adds 1
+
+// count--, count = count - 1 — subtracts 1
 
 public class WhileLoop {
 
     public static void countDown() {
         int countdown = 10;
         while (countdown > 0) {
-            System.out.println(countdown);
+            System.out.println("I LOVE YOU: " + countdown);
             countdown--;
 
-            // count++ // count = count + 1 — adds 1
-            // count = count - 1 — subtracts 1
         }
-        System.out.println("Happy Birthday");
+
+    }
+
+    public static void falseCondition() {
+
+        int numbers = 10;
+
+        while (numbers < 5) {
+            System.out.println("This will never be printed");
+            numbers++;
+
+        }
+
     }
 
     public static void main(String[] args) {
 
-        countDown();
+        // countDown();
+        falseCondition();
     }
 
 }
